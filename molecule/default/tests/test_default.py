2bebad039b62ab3c43594bd8861c4e10a80f7c2c
Alex Kotov <kotovalexarian@gmail.com>
2020-01-14 15:55:56 +0500

    Add "tasks/certbot.yml"

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -54,3 +54,23 @@ def test_iptables_config(host, version):
 def test_nginx_default_removed(host):
     assert host.file('/etc/nginx/sites-available/default').exists
     assert not host.file('/etc/nginx/sites-enabled/default').exists
+
+
+def test_certbot_cli_config(host):
+    f = host.file('/etc/letsencrypt/cli.ini')
+
+    assert f.exists
+    assert f.is_file
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.mode == 0o644
+
+
+def test_certbot_nginx_config(host):
+    f = host.file('/etc/letsencrypt/options-ssl-nginx.conf')
+
+    assert f.exists
+    assert f.is_file
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.mode == 0o644
