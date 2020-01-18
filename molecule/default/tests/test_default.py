c526ee4fc17fe17030a2fc4b1d3bd409d8278f6d
Alex Kotov <kotovalexarian@gmail.com>
2020-01-18 15:23:56 +0500

    Install system package "procps"

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -19,6 +19,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
         'gnupg2',
         'iptables-persistent',
         'less',
+        'procps',
         'software-properties-common',
         'vim',
         'wget',
